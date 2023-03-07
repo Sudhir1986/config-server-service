2ade87ded2763de33f914a04da9e90c6efddae37
Sudhir Krishna <sudhirkrishna@192.168.1.20>
2023-03-07 12:47:48 +0530

    first commit

---
+++ src/main/java/com/infotech/configserver/ConfigServerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.infotech.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerServiceApplication.class, args);
+	}
+
+}
